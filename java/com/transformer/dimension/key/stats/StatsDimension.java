97dae31e7a8953d344c19cddaef0293fa262da94
Lee Dongliang <31875188+LuciferCoder@users.noreply.github.com>
2019-03-04 15:30:59 +0800

    hdfs-hbase-hadoop-journal-analyse

---
+++ java/com/transformer/dimension/key/stats/StatsDimension.java
@@ -0,0 +1,11 @@
+package com.transformer.dimension.key.stats;
+
+import com.transformer.dimension.key.BaseDimension;
+
+/**
+ * 组合维度类的基类
+ *
+ */
+public abstract class StatsDimension extends BaseDimension {
+    // nothing
+}
